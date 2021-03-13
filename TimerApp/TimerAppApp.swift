6c6afa103bfc146ff92a2813bc089cd39d9fb988
Elechim01 <mikymanny015@gmail.com>
2021-03-13 17:36:13 +0100

    Initial Commit

---
+++ TimerApp/TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp
+//
+//  Created by Michele Manniello on 13/03/21.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
